21dbd046f4ead59538e0a4d8df4a39c801282bd9
lkzn <lkznstartup@live.com>
2013-07-15 14:23:02 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worl312312312312312312313123123');
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
